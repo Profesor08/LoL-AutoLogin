e42e8f548ac4b44e854ed63ec983d3ab74d35d6f
Profesor08 <online7890@gmail.com>
2017-02-07 03:55:24 +0200

    Update to version 1.1

--- LoL AutoLogin/Program.cs
+++ LoL AutoLogin/Program.cs
@@ -8,56 +8,37 @@ namespace LoL_AutoLogin
     class Program
     {
 
-        [DllImportAttribute("User32.dll")]
-        private static extern int FindWindow(String ClassName, String WindowName);
-
-        [DllImport("User32.dll")]
-        private static extern bool ShowWindowAsync(IntPtr hWnd, int cmdShow);
-
-        [DllImport("User32.dll")]
-        private static extern bool SetForegroundWindow(IntPtr hWnd);
-
-        [DllImport("User32.dll")]
-        public static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
-
-        private const int WS_SHOWNORMAL = 1;
-
         static void Main(string[] args)
         {
-            if (args.Length == 2)
+            //args = new string[] {"7000", "123", "C:\\Games\\League of Legends\\" };
+            if (args.Length == 3)
             {
                 int timeout = int.Parse(args[0]);
-                string passowrd = args[1];
+                string password = args[1];
+                string gamePath = args[2];
 
-                Process process = new Process();
-                // Configure the process using the StartInfo properties.
-                process.StartInfo.FileName = "LeagueClient.exe";
-                process.StartInfo.Arguments = "-n";
-                process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
-                process.Start();
+                LeagueClient client = new LeagueClient(gamePath, "LeagueClient.exe", "LeagueClientUx");
 
-                // wait for process loads
-                System.Threading.Thread.Sleep(timeout);
-
-                Process[] localByName = Process.GetProcessesByName("LeagueClientUx");
-
-                foreach (var p in localByName)
+                try
                 {
-                    SetForegroundWindow(p.MainWindowHandle);
-                    ShowWindowAsync(p.MainWindowHandle, WS_SHOWNORMAL);
-                    System.Threading.Thread.Sleep(500);
-                    // clearing input field
-                    SendKeys.SendWait("^{a}");
-                    SendKeys.SendWait(passowrd);
-                    SendKeys.SendWait("{ENTER}");
-                    Application.Exit();
+                    if (!client.IsRunning())
+                    {
+                        client.Start();
+                        System.Threading.Thread.Sleep(timeout);
+                    }
+                    
+                    client.Login(password);
+                }
+                catch (System.ComponentModel.Win32Exception ex)
+                {
+                    MessageBox.Show(ex.Message + ": " + client.GetClientFile(), "AutoLogin", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
             else
             {
-                Console.WriteLine("Set parameters: [timeout] [password]");
-                Console.ReadLine();
+                MessageBox.Show("Set parameters: [timeout] [password] [gamepath]", "AutoLogin", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+
         }
     }
 }
